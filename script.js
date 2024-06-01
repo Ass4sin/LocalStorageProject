bda886498b01a220e96c9f64a721296ce92678d2
Ass4sin <tudorandrei2010@yahoo.com>
2024-06-01 13:17:49 +0200

    working register button

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ $("#add").on("click",function(){
     }
 });
 
+$("#submitButton").on("click", function() {
+    const surname = document.querySelector("#Surname").value;
+    const firstName = document.querySelector("#firstName").value;
+    const phoneNumber = document.querySelector("#Number").value;
+    
+    const register = {
+        surname: surname,
+        firstName: firstName,
+        phoneNumber: phoneNumber
+    };
+
+    let information = window.localStorage.getItem("Informations");
+    console.log(information);
+    let arr = [];
+    if (information !== null) {
+        arr = JSON.parse(information);
+    }
+
+    arr.push(register);
+
+    window.localStorage.setItem("Informations", JSON.stringify(arr));
+
+});

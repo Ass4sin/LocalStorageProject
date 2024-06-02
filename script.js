b35b919fc52eb217c4bf02771578c2e290f86183
Ass4sin <129887225+Ass4sin@users.noreply.github.com>
2024-06-02 23:06:01 +0200

    trying to check existing contacts

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const ul = document.querySelector("ul")
+
+
 $("#add").on("click",function(){
     if ($(".informations").css("display") == "none") {
         $(".informations").show("slow");
@@ -5,6 +8,11 @@ $("#add").on("click",function(){
         $(".informations").hide("slow");
     }
 });
+
+
+$("#erase").on("click", function(){
+    localStorage.clear()
+})
 
 $("#submitButton").on("click", function() {
     const surname  = document.querySelector("#Surname").value
@@ -18,14 +26,44 @@ $("#submitButton").on("click", function() {
     }
 
     let arr = []
-
+    // let namesArr = []
     information = localStorage.getItem("Information")
+    // names = localStorage.getItem("Names")
 
     if (information !== null) {
         arr = JSON.parse(information)
+        // namesArr = JSON.parse(names)
     }
-
+    
+    
+    
+    
     arr.push(register)
+    // namesArr.push(surname, firstName)
+    
+    
+    ul.appendChild(document.createElement("li")).textContent = surname + " " + firstName; //need to be able to stock this new list in the local storage
     
     localStorage.setItem("Information", JSON.stringify(arr))
+    // localStorage.setItem("Names", JSON.stringify(namesArr))
+    checkInformations(register)
 });
+
+
+function checkInformations(data) {
+    console.log(localStorage.length);
+    if(localStorage.length !== 0){
+        getLocalStorage = JSON.parse(localStorage.getItem("Information"))
+        localStorageLength = getLocalStorage.length
+
+        for(let i = 0; i < localStorageLength; i++){
+            if (getLocalStorage[i].surname === data.surname && getLocalStorage[i].firstName === data.firstName && getLocalStorage[i].phoneNumber === data.phoneNumber) {
+                //working if, not to figure out how to not add the data
+                localStorage.removeItem("Information", data)
+            }
+        }
+    } else{
+        console.log("empty local storage for now");
+    }
+    
+}

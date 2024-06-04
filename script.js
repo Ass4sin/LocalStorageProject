452852bb4c5de41f6b94caf88829819abdba090e
Ass4sin <129887225+Ass4sin@users.noreply.github.com>
2024-06-04 20:02:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const ul = document.querySelector("ul")
-
-
+const select = document.querySelector("#gender")
+let isEditing = -10
 $("#add").on("click", function () {
     if ($(".informations").css("display") == "none") {
         $(".informations").show("slow");
@@ -9,12 +9,16 @@ $("#add").on("click", function () {
     }
 });
 
+$(".input").on("click", function (event) {
+    event.target.value = ""
+})
 
 $("#erase").on("click", function () {
     localStorage.clear()
 })
 
 $("#submitButton").on("click", function () {
+    // debugger
     const surname = document.querySelector("#Surname").value
     const firstName = document.querySelector("#firstName").value
     const phoneNumber = document.querySelector("#Number").value
@@ -25,10 +29,12 @@ $("#submitButton").on("click", function () {
         phoneNumber: phoneNumber
     }
 
+    if (register.surname === "" || register.firstName === "" || register.phoneNumber === "") {
+        alert("You must fill in the blanks")
+        return
+    }
+
     let arr = []
-    // let namesArr = []
-    // information = localStorage.getItem("Information")
-    // names = localStorage.getItem("Names")
 
     if (localStorage.length === 0) {
         arr.push(register)
@@ -44,23 +50,59 @@ $("#submitButton").on("click", function () {
         }
     }
 
+    if (isEditing !== -10) {
+        getLocalStorage = JSON.parse(localStorage.getItem("Information"))
+        getLocalStorage[isEditing].surname === surname
+        getLocalStorage[isEditing].firstName === firstName
+        getLocalStorage[isEditing].phoneNumber === phoneNumber
+    }
+
+    document.querySelector("#Surname").value = ""
+    document.querySelector("#firstName").value = ""
+    document.querySelector("#Number").value = ""
+    isEditing = -10
+    showNames()
+    console.log(isEditing);
 });
 
-
-function showNames() {
-    const surname = document.querySelector("#Surname").value
-    const firstName = document.querySelector("#firstName").value
-    const phoneNumber = document.querySelector("#Number").value
-    if (localStorage.length === 0){
-        return
-    }
+function modifyUser(user) {
+    let isEditing = user
     getLocalStorage = JSON.parse(localStorage.getItem("Information"))
-    len = getLocalStorage.length
-    for(let i = 0; i < len; i++){
-        names = ul.appendChild(document.createElement("li"))
-        names.innerHTML += getLocalStorage[i].surname
+    getLocalStorage[user]
+    document.querySelector("#Surname").value = getLocalStorage[user].surname
+    document.querySelector("#firstName").value = getLocalStorage[user].firstName
+    document.querySelector("#Number").value = getLocalStorage[user].phoneNumber
+}
+
+function showNames(deleteUser) {
+    if (!deleteUser) {
+        if (localStorage.length === 0) {
+            return
+        }
+        getLocalStorage = JSON.parse(localStorage.getItem("Information"))
+        len = getLocalStorage.length - 1
+        newList = document.createElement("li")
+        newList.setAttribute("id", `listItem_${len}`)
+        newList.setAttribute("onclick", `modifyUser(${len})`)
+        names = ul.appendChild(newList)
+        names.classList.add("contactList")
+        names.innerHTML += getLocalStorage[len].surname + " " + getLocalStorage[len].firstName + " " + getLocalStorage[len].phoneNumber + `<i class="fa-solid fa-trash" onclick="deleteUser(${len})"></i>`
+    } else {
+        toRemove = document.querySelector(`#listItem_${deleteUser}`)
+        ul.removeChild(toRemove)
     }
 }
+
+function deleteUser(user) {
+    // debugger
+    getLocalStorage = JSON.parse(localStorage.getItem("Information"))
+    if (Array.isArray(getLocalStorage)) {
+        getLocalStorage.splice(user, 1)
+        localStorage.setItem("Information", JSON.stringify(getLocalStorage))
+        showNames(user)
+    }
+}
+
 
 function checkInformations(data) {
     if (localStorage.length !== 0) {
@@ -77,5 +119,3 @@ function checkInformations(data) {
     }
 
 }
-
-showNames()
